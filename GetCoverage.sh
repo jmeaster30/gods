4fb387fade6fed220eac72a9a93a6edc58b364a8
jmeaster30 <jmeasterday@comcast.net>
2023-09-06 22:25:22 -0400

    Fixed getopts thing with coverage helper script

--- GetCoverage.sh
+++ GetCoverage.sh
@@ -2,7 +2,7 @@
 go test -v -coverprofile="coverage.text" ./aviary ./composite ./linear
 go tool cover -html="coverage.text" -o "coverage.html"
 
-while getopts o: arg;
+while getopts o arg;
 do
   case $arg in
     o) 
